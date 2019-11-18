ebb9098f097cc0d04861600bf2bd66e523be62a6
Philip Klostermann <philip.klostermann@gmail.com>
2019-11-17 21:26:10 -0500

    Add meta information

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,18 +4,17 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Philip Klostermann - Resume',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'Mobile and web developer resume.'
       }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
         href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css'
